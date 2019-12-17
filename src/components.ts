ef937279f8307580b8e71d4a5e9f5e3e8f302a18
Sil van Diepen <me@silvandiepen.com>
2019-12-17 13:52:07 +0100

    init

---
+++ src/components.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from 'hv-styleguide';
+
+import { defineCustomElements, applyPolyfills } from 'hv-styleguide/loader';
+
+applyPolyfills().then(() => { defineCustomElements(window); });
+export const HvButton = /*@__PURE__*/createReactComponent<JSX.HvButton, HTMLHvButtonElement>('hv-button');
+export const InputField = /*@__PURE__*/createReactComponent<JSX.InputField, HTMLInputFieldElement>('input-field');
