9ccca80db0396e4f5e04c6e5d533b80d98969337
Sil van Diepen <me@silvandiepen.com>
2019-12-19 08:35:08 +0100

    add main file

--- src/components.ts
+++ src/components.ts
@@ -1,12 +1,20 @@
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
-import { createReactComponent } from './react-component-lib';
+import { createReactComponent } from "./react-component-lib";
 
-import { JSX } from 'hv-styleguide';
+import { JSX } from "hv-styleguide";
 
-import { defineCustomElements, applyPolyfills } from 'hv-styleguide/loader';
+import { defineCustomElements, applyPolyfills } from "hv-styleguide/loader";
 
-applyPolyfills().then(() => { defineCustomElements(window); });
-export const HvButton = /*@__PURE__*/createReactComponent<JSX.HvButton, HTMLHvButtonElement>('hv-button');
-export const HvInputText = /*@__PURE__*/createReactComponent<JSX.HvInputText, HTMLHvInputTextElement>('hv-input-text');
+applyPolyfills().then(() => {
+	defineCustomElements(window);
+});
+export const HvButton = /*@__PURE__*/ createReactComponent<
+	JSX.HvButton,
+	HTMLHvButtonElement
+>("hv-button");
+export const HvInputText = /*@__PURE__*/ createReactComponent<
+	JSX.HvInputText,
+	HTMLHvInputTextElement
+>("hv-input-text");
