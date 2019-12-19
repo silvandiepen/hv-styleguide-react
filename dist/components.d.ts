8c04bc463e274d87463530aa66a3a70ed16a6b6d
Sil van Diepen <me@silvandiepen.com>
2019-12-19 08:52:32 +0100

    add base template

---
+++ dist/components.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { JSX } from 'hv-styleguide';
+export declare const HvButton: import("react").ForwardRefExoticComponent<JSX.HvButton & import("react").HTMLAttributes<HTMLHvButtonElement> & import("./react-component-lib/utils").ReactProps & import("react").RefAttributes<HTMLHvButtonElement>>;
+export declare const HvInputText: import("react").ForwardRefExoticComponent<JSX.HvInputText & import("react").HTMLAttributes<HTMLHvInputTextElement> & import("./react-component-lib/utils").ReactProps & import("react").RefAttributes<HTMLHvInputTextElement>>;
--- dist/components.js
+++ dist/components.js
@@ -2,11 +2,8 @@
 /* tslint:disable */
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
-
-import { JSX } from 'hv-styleguide';
-
 import { defineCustomElements, applyPolyfills } from 'hv-styleguide/loader';
-
 applyPolyfills().then(() => { defineCustomElements(window); });
-export const HvButton = /*@__PURE__*/createReactComponent<JSX.HvButton, HTMLHvButtonElement>('hv-button');
-export const HvInputText = /*@__PURE__*/createReactComponent<JSX.HvInputText, HTMLHvInputTextElement>('hv-input-text');
+export const HvButton = /*@__PURE__*/ createReactComponent('hv-button');
+export const HvInputText = /*@__PURE__*/ createReactComponent('hv-input-text');
+//# sourceMappingURL=components.js.map
